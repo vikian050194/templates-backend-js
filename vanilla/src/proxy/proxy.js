6c75018cd6bea451a28c6bb01babce1a2686e431
Kirill Vinogradov <vikian050194@gmail.com>
2023-09-19 11:43:51 +0200

    use c8 instead of nyc

---
+++ vanilla/src/proxy/proxy.js
@@ -0,0 +1,72 @@
+const PORT = process.env.PORT || 8000;
+const PWD = process.env.PWD;
+
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+const dir = "src";
+const isApi = new RegExp(/\/api\/(.*)/);
+
+const backend = {
+    host: "127.0.0.1",
+    port: 8080
+};
+
+const getMimeType = (ext) => {
+    const types = {
+        "css": "text/css",
+        "html": "text/html",
+        "js": "text/javascript",
+        "json": "application/json",
+        "svg": "image/svg+xml"
+    };
+
+    return types[ext];
+};
+
+http.createServer((client_req, client_res) => {
+    console.info(`Request: ${client_req.url}`);
+
+    const u = url.parse(client_req.url);
+    const match = isApi.exec(u.pathname);
+
+    if (match) {
+        const options = {
+            hostname: backend.host,
+            port: backend.port,
+            path: "/" + match[1],
+            method: client_req.method,
+            headers: client_req.headers
+        };
+
+        const proxy = http.request(options, (res) => {
+            client_res.writeHead(res.statusCode, res.headers);
+            res.pipe(client_res, {
+                end: true
+            });
+        });
+
+        client_req.pipe(proxy, {
+            end: true
+        });
+    } else {
+        const file = client_req.url === "/" ? "/index.html" : client_req.url;
+        
+        console.info(`File: ${dir}${file}`);
+        
+        const ext = file.split(".")[1];
+        const path = `${PWD}/${dir}${file}`;
+        fs.access(path, fs.F_OK, (err) => {
+            if (err) {
+                client_res.statusCode = 404;
+                client_res.end();
+            } else {
+                client_res.setHeader("Content-Type", getMimeType(ext));
+                fs.createReadStream(path, "utf8").pipe(client_res);
+            }
+        });
+    }
+}).listen(PORT, () => {
+    console.info("Server is listening on port", PORT);
+});
--- vanilla/src/proxy/static.js
+++ vanilla/src/proxy/static.js
@@ -2,16 +2,9 @@ const PORT = process.env.PORT || 8000;
 const PWD = process.env.PWD;
 
 const http = require("http");
-const url = require("url");
 const fs = require("fs");
 
-const dir = "src";
-const isApi = new RegExp(/\/api\/(.*)/);
-
-const backend = {
-    host: "127.0.0.1",
-    port: 8080
-};
+const dir = "build";
 
 const getMimeType = (ext) => {
     const types = {
@@ -26,47 +19,23 @@ const getMimeType = (ext) => {
 };
 
 http.createServer((client_req, client_res) => {
-    console.info(client_req.url);
+    console.info(`Request: ${client_req.url}`);
 
-    const u = url.parse(client_req.url);
-    const match = isApi.exec(u.pathname);
+    const file = client_req.url === "/" ? "/index.html" : client_req.url;
 
-    if (match) {
-        const options = {
-            hostname: backend.host,
-            port: backend.port,
-            path: "/" + match[1],
-            method: client_req.method,
-            headers: client_req.headers
-        };
+    console.info(`File: ${dir}${file}`);
 
-        const proxy = http.request(options, (res) => {
-            client_res.writeHead(res.statusCode, res.headers);
-            res.pipe(client_res, {
-                end: true
-            });
-        });
-
-        client_req.pipe(proxy, {
-            end: true
-        });
-    } else {
-        const file = client_req.url === "/" ? "/index.html" : client_req.url;
-        
-        console.info(`loading file: ${dir}${client_req.url}`);
-        
-        const ext = file.split(".")[1];
-        const path = `${PWD}/${dir}${file}`;
-        fs.access(path, fs.F_OK, (err) => {
-            if (err) {
-                client_res.statusCode = 404;
-                client_res.end();
-            } else {
-                client_res.setHeader("Content-Type", getMimeType(ext));
-                fs.createReadStream(path, "utf8").pipe(client_res);
-            }
-        });
-    }
+    const ext = file.split(".")[1];
+    const path = `${PWD}/${dir}${file}`;
+    fs.access(path, fs.F_OK, (err) => {
+        if (err) {
+            client_res.statusCode = 404;
+            client_res.end();
+        } else {
+            client_res.setHeader("Content-Type", getMimeType(ext));
+            fs.createReadStream(path, "utf8").pipe(client_res);
+        }
+    });
 }).listen(PORT, () => {
     console.info("Server is listening on port", PORT);
 });
